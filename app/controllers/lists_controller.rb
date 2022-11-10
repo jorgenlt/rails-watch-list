2289bcc41fcd01b79f2ee2fa7019805c3d8fa439
Joergen Tjoennteig <jorgenlt@outlook.com>
2022-11-10 17:15:40 +0800

    save

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list.id)
+    # redirect_to root_path
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  end
