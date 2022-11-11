a6602c80822eff53349dd2181d2dd3b08ef2a879
Joergen Tjoennteig <jorgenlt@outlook.com>
2022-11-11 08:23:25 +0800

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +8,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+
+# 100.times do
+#   Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://picsum.photos/200/300?random=#{rand(20..70)}", rating: rand(1..9))
+#   puts 'movie created'
+# end
+# puts 'finished'
